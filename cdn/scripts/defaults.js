f7ac22f260124f5d7164741af2beb1b082bbe4d9
Catos <119569640+catosoriginal@users.noreply.github.com>
2026-04-04 21:16:00 +0300

    teewt

--- cdn/scripts/defaults.js
+++ cdn/scripts/defaults.js
@@ -1,3 +1,6 @@
 
-console.log("Default module loaded", self.src)
-console.info("Contact me on mail: catosoriginal@proton.me")
+
+console.log("Default module loaded", self.src);
+console.info("Contact me on mail: catosoriginal@proton.me");
+
+import footer from "/epiroc-safety/cdn/assets/pageelements/footer/footer.html"
